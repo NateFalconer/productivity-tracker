f3a5d6e45502870870995f8bf6da4d4525af560f
NateFalconer <natefalconer@protonmail.com>
2022-08-15 14:26:38 -0400

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from '../components/Navbar'
+import { AuthContextProvider } from '../context/AuthContext'
+
+// const noAuthRequired = ['/', 'login', '/signup']
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthContextProvider>
+      <Navbar />
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  )
 }
 
 export default MyApp
